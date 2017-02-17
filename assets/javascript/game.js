91d52f1ee826d7f30f30239030948faa9ce1891a
dashaman <hang.andy.t@gmail.com>
2017-02-16 23:16:15 -0600

    v0.03

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,18 @@
 //Execute after DOM is ready
 $(document).ready(function() {
+    var myChar;
+    var enemyCharacter;
+
+    var win = 0;
+    var lose = 0;
+
 //Set Global Variables for playerCharacter, enemyCharacter, win counter? lose counter?,
 //Game Object for all characters
 //Need to determine when the player select character it becomes theirs and then determine when
 //the player is select the enemy to fight. .on("Click" function())
 //Need to be have player select Attack and enemy to counterAttack for each attack
-//Reset Function!
+//What happens when enemy character gets defeated
+//Reset Function! <restart button> <attack button> 5 rows
 //
 
 })
