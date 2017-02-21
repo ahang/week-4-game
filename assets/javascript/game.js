63ad4d8bfd0c7cee6df1605f42d84a72ab452395
dashaman <hang.andy.t@gmail.com>
2017-02-20 20:18:04 -0600

    v0.7

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var playerCharacter;
     var enemyCharacter;
     var selectedCharacter = false;
-    var selectedDefender = false;
+    var selectDefender = false;
 
     // var playerChar;
     // var enemyCharacter;
@@ -16,8 +16,7 @@ $(document).ready(function() {
     // var lose = 0;
 
     //set constructor charactersAttribute
-    var charactersAttr = function(id, name, attack, hitpoints, pic) {
-        this.id = id;
+    var charactersAttr = function(name, attack, hitpoints, pic) {
         this.name = name;
         this.attack = attack;
         this.hitpoints = hitpoints;
@@ -25,10 +24,10 @@ $(document).ready(function() {
     }
     //setup the fighters in an object array
     var fighter = [
-        new charactersAttr(1, "Aang", 50, 1000, '<img src="assets/images/Aang.png">'),
-        new charactersAttr(2, "Zuko", 100, 100, '<img src="assets/images/Zuko.png">'),
-        new charactersAttr(3, "Katara", 50, 1000, '<img src="assets/images/Katara.png">'),
-        new charactersAttr(4, "Sokka", 50, 1000, '<img src="assets/images/Toph.png">')
+        new charactersAttr("Aang", 20, 180, '<img src="assets/images/Aang.png">'),
+        new charactersAttr("Zuko", 25, 150, '<img src="assets/images/Zuko.png">'),
+        new charactersAttr("Katara", 18, 175, '<img src="assets/images/Katara.png">'),
+        new charactersAttr("Toph", 17, 200, '<img src="assets/images/Toph.png">')
     ];
     // console.log(fighter[1]);
 
@@ -43,56 +42,62 @@ $(document).ready(function() {
 
 //Set up the start of the game
     function initializeGame() {
+        playerCharacter;
+        enemyCharacter;
+        selectedCharacter = false;
+        selectDefender = false;
         //loops through the available fighters and appends them onto the selectCharacter div
-        //
         for (var i = 0; i < fighter.length; i++) {
             console.log("This loop is starting");
             var selectDiv = $("<div>");
-            selectDiv.addClass("btn charSelect"); //add the btn class/charSelect for each character
+            selectDiv.addClass("btn charSelect text-center"); //add the btn class/charSelect for each character
             console.log(fighter[i].name + " You are now a button");
-            selectDiv.html(fighter[i].pic);
+            selectDiv.html(fighter[i].pic + "<br>" + fighter[i].name + "<br>HP: " + fighter[i].hitpoints +
+                "<br>Attack: " + fighter[i].attack);
             console.log(fighter[i].pic); //checking
 
             $(".selectCharacters").append(selectDiv); //appends a div inbetween the selectCharacters html div
 
             console.log("Appended character: " + fighter[i].name);
         }
+        $(".action-bar").html("Choose a character");
+        selectCharacter();
+        attack();
+        reset();
         console.log("Completed Initializing");
 
     }
 
 initializeGame();
 
-$(".charSelect").on("click", function() {
-    //$(this).toggleClass("clicked"); //test to see if each char gets clicked
-    //IF statement to check if the playerSelected the character.
-    if (!selectedCharacter) {
-        playerCharacter = $(this).attr("div");
-        $(".playerCharacter").append(this);
-        $(this).addClass("player");
-        console.log("You have selected a character");
-        selectedCharacter = true;
-    } //Checking to see if Defender has been selected yet
-    else if (!selectedDefender && selectedCharacter !== playerCharacter) {
-        enemyCharacter = $(this).attr("div");
-        $(".enemyCharacters").append(this);
-        console.log("You have selected an opponent");
-        selectedDefender = true;
-    }
-    // var movePlayer = $("<div>");
-    // movePlayer.addClass("playerChar");
-    // movePlayer.removeClass("charSelect");
-    // $(".playerCharacter").appendTo($(this));
-    // $(".playerCharacter").append(movePlayer);
-    // $(".charSelect").children().appendTo(".enemyCharacters");
-
-
-});
-
 //Have the user Selects the character
 //Applies character to the selected character and applies enemy class to the unchosen character
-
+//Bug - SelectedCharacter can still be selected to go to OpponentCharacter.. - need to fix
     function selectCharacter() {
+        $(".charSelect").on("click", function() {
+        //$(this).toggleClass("clicked"); //test to see if each char gets clicked
+        //IF statement to check if the playerSelected the character.
+            if (!selectedCharacter) {
+                playerCharacter = $(this).attr("div");
+                $(".playerCharacter").append(this);
+                $(this).addClass("player");
+                console.log("You have selected a character");
+                selectedCharacter = true;
+                $(".action-bar").html("Choose your opponent!");
+            } //Checking to see if Defender has been selected yet
+            else if (!selectDefender && selectedCharacter !== $(this).attr("div")) {
+                enemyCharacter = $(this).attr("div");
+                $(".enemyCharacters").append(this);
+                $(this).addClass("opponent");
+                console.log("You have selected an opponent");
+                selectDefender = true;
+                $(".action-bar").html("Click attack to fight your opponent");
+            }
+        });
+    }
+
+    function combatInfo() {
+
 
     }
 
@@ -103,15 +108,28 @@ $(".charSelect").on("click", function() {
     }
 
 //This will allow the user to attack once the enemyCharacter is in the defender field
+//Checks to make sure the character has been selected and the defender has been selected before
+//allowing the user to hit the attack button
     function attack() {
+        $(".attack").on("click", function() {
+            console.log("I am attacking");
+            if (!selectedCharacter) {
+                $(".combat-log").html("Please select a character");
+            } else if(!selectDefender) {
+                $(".combat-log").html("Please select an opponent");
+            } else if (selectedCharacter && selectDefender) {
+                $(".combat-log").html("I am attacking");
+            }
+        })
 
     }
-//This will determine the counterAttack of the defender
-    function defend() {
-
+//resetting
+    function reset() {
+        $(".reset").on("click", function() {
+            console.log("I am trying to reset");
+            initializeGame();
+        })
     }
-
-
-
 
 });
+
